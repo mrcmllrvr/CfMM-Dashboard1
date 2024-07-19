ad4176f197351990a6b005c194076b313fa4be31
Maria Camille Rivera <130654405+mrcmllrvr@users.noreply.github.com>
2024-07-19 15:37:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,6 @@ st.sidebar.markdown("[Chart](#)")
 st.sidebar.markdown("[Level](#)")
 st.sidebar.markdown("[Badge Items](#)")
 
-# Main content
-st.markdown("<Date Today>")
-st.title("Today's Insight/Metrics")
-
 # Embed the Dash app using an iframe
 dash_app_url = "http://127.0.0.1:8050/"  # URL where the Dash app is running
 st.components.v1.iframe(dash_app_url, width=2100, height=30000)
