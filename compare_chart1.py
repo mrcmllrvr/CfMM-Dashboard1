c10e7c37a4e630415f8c9cc1d7ba101fb0faec83
Maria Camille Rivera <130654405+mrcmllrvr@users.noreply.github.com>
2024-09-30 12:21:43 +0800

    Update compare_chart1.py
    updated UI:
    - rounded borders: compare view

--- compare_chart1.py
+++ compare_chart1.py
@@ -180,7 +180,7 @@ def create_layout():
                 dbc.Button('Export to CSV', id='export-button1a', style = {'display': 'none'})
             ], style={'display':'flex', 'margin-top': '10px', 'align-items': 'center'}),
         ],
-        style={'width': '48%', 'display': 'inline-block', 'backgroundColor': 'white', 'box-shadow': '0 8px 15px rgba(0, 0, 0, 0.2)', 'border-radius': '0px', 'padding': '10px', 'margin': '5px'}),
+        style={'width': '48%', 'display': 'inline-block', 'backgroundColor': 'white', 'border': '2px solid #d3d3d3', 'border-radius': '8px', 'padding': '10px', 'margin': '5px'}),
 
         # All elements for Chart 1B
         html.Div([
@@ -285,7 +285,7 @@ def create_layout():
                 dbc.Button('Export to CSV', id='export-button1b', style = {'display': 'none'})
             ], style={'display':'flex', 'margin-top': '10px', 'align-items': 'center'}),
         ],
-        style={'width': '48%', 'display': 'inline-block', 'backgroundColor': 'white', 'box-shadow': '0 8px 15px rgba(0, 0, 0, 0.2)', 'border-radius': '0px', 'padding': '10px', 'margin': '5px'}),
+        style={'width': '48%', 'display': 'inline-block', 'backgroundColor': 'white', 'border': '2px solid #d3d3d3', 'border-radius': '8px', 'padding': '10px', 'margin': '5px'}),
     ])
 
     return layout
